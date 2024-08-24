1a9c12c2c93231cedb8de67332a3586dab121995
pedro-young-wedbush <103218764+pedro-young-wedbush@users.noreply.github.com>
2024-08-24 09:54:29 -0400

    Updates

--- src/index.tsx
+++ src/index.tsx
@@ -1,35 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { IOConnectProvider } from "@interopio/react-hooks";
-import Glue from "@glue42/desktop";
-import IOBrowserPlatform, { IOConnectBrowserPlatform } from "@interopio/browser-platform";
-import IOWorkspaces from "@interopio/workspaces-api";
-import config from "./config.json";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-console.log("🚀 ~ config:", config)
-
-ReactDOM.render(
-  <React.StrictMode>
-    <IOConnectProvider settings={{
-      browserPlatform: {
-        config: Object.assign({}, config.browserPlatform, { browser: { libraries: [IOWorkspaces] }, serviceWorker: { url: "/service-worker.js" } }) as IOConnectBrowserPlatform.Config,
-        factory: IOBrowserPlatform
-      },
-      desktop: {
-        config: { libraries: [IOWorkspaces], appManager: "skipIcons" },
-        factory: Glue
-      }
-    }}>
-      <App />
-    </IOConnectProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
