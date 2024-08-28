160c72d4a99a5121edfb6a3ef54f8943fbf9fc39
WaghK <118017397+WaghK@users.noreply.github.com>
2024-08-28 12:34:48 +0530

    Updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import viteTsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    viteTsconfigPaths(),],
   server: {
     open: true, 
   },
   build: {
-    outDir: 'build', 
+    outDir: 'build',
+    target: 'esnext',
   },
 });
+
